6368d88ead36050a6ab5b401c9d5d944c05300e0
imniloy <imniloy8@gmail.com>
2024-07-08 19:36:16 +0600

    some code modified for readablity

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+// Code for capturing some words of about-section that will be highlighted so I add about-section_display-text-highlight
+
 document.addEventListener("DOMContentLoaded", function () {
     const textElement = document.getElementById("about-section_display-text");
     const specificWords = [
@@ -18,35 +20,3 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-
-// let currentIndex = 0;
-
-// const slides = document.querySelectorAll('.slider-section__image-slide');
-// const totalSlides = slides.length;
-// const prevButton = document.querySelector('.prev');
-// const nextButton = document.querySelector('.next');
-
-// function showSlide(index) {
-//     if (index >= totalSlides) {
-//         currentIndex = 0;
-//     } else if (index < 0) {
-//         currentIndex = totalSlides - 1;
-//     } else {
-//         currentIndex = index;
-//     }
-//     const newTransform = `translateX(-${currentIndex * (456 + 20)}px)`; // 456 is the slide width, 20 is the gap
-//     document.querySelector('.slider-section__slide-container').style.transform = newTransform;
-// }
-
-// prevButton.addEventListener('click', () => {
-//     showSlide(currentIndex - 1);
-// });
-
-// nextButton.addEventListener('click', () => {
-//     showSlide(currentIndex + 1);
-// });
-
-// // Optionally, you can add auto-slide functionality:
-// setInterval(() => {
-//     showSlide(currentIndex + 1);
-// }, 5000); // Change slide every 5 seconds
